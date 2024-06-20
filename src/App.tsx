ac4012ed6ecd5e2baea381ed6b897d460cdca5de
dimabreus <dmitriybreus5@gmail.com>
2024-06-21 00:59:02 +0300

    Added line

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React from 'react';
+import React, { useRef } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Test from './components/Test';
@@ -8,13 +8,23 @@ import { RootState } from './store/store';
 import TreeTask from './components/treeTask/TreeTask';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import Xarrow from 'react-xarrows';
+
+const boxStyle = {border: "grey solid 2px", borderRadius: "10px", padding: "5px", width: '5px'};
 
 const App: React.FC = () => {
   const root = useSelector((state: RootState) => state.tree.root);
-
   return (
     <Router>
-      <Header />
+      <div id="elem1" style={{...boxStyle, marginBottom: '50px', marginLeft: '550px'}}>hey</div>
+      <div id="elem2" style={boxStyle}>hey2</div>
+      <Xarrow
+        start="elem1"
+        end="elem2"
+        path='grid'
+        showHead={false}
+      />
+      {/* <Header />
 
       <div className='wrapper'>
         <div className="navBar">
@@ -29,7 +39,7 @@ const App: React.FC = () => {
             <Route path="/*" element={<Test />} />
           </Routes>
         </div>
-      </div>
+      </div> */}
     </Router>
   );
 };
